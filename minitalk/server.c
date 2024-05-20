c1083850e1824b781400f142b99aa2cf139d9100
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-05-20 19:21:55 +0300

    Update server.c

--- minitalk/server.c
+++ minitalk/server.c
@@ -1,18 +1,6 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: alerkul <alerkul@student.42istanbul.com.t  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/29 18:13:01 by alerkul           #+#    #+#             */
-/*   Updated: 2024/04/29 18:13:03 by alerkul          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minitalk.h"
 
-void 	print_pid(pid_t pid)
+static void	print_pid(pid_t pid)
 {
 	char	i;
 
@@ -28,33 +16,35 @@ void 	print_pid(pid_t pid)
 	}
 }
 
-void signal_handler(int signal) {
-    static char chr = 0;
-    static int bit = 0;
+static void	signal_handler(int signal)
+{
+	static char	chr = 0;
+	static int	bit = 0;
 
-
-    chr = (chr << 1) | (signal == SIGUSR1 ? 1 : 0);
-    bit++;
-
-    if(bit == 8) 
-    {
-        write(1, &chr, 1);
-        bit = 0;
-        chr = 0;
-    }
+	if (signal == SIGUSR1)
+		chr = (chr << 1) | (1);
+	if (signal == SIGUSR2)
+		chr = (chr << 1) | (0);
+	bit++;
+	if (bit == 8)
+	{
+		write(1, &chr, 1);
+		bit = 0;
+		chr = 0;
+	}
 }
 
-int main()
+int	main(void)
 {
-    pid_t process_id;
+	pid_t	process_id;
 
-    process_id = getpid();
-	print_pid(process_id);	
-    signal(SIGUSR1, signal_handler);
-    signal(SIGUSR2, signal_handler);
-    while (1)
+	process_id = getpid();
+	print_pid(process_id);
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
+	while (1)
 	{
-		pause();   //sinyal gelene kadar
+		pause();
 	}
-    return 0;
+	return (0);
 }
